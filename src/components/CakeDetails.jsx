40775ed404b61cf84868407f1b284804cf1bf9cc
saloni-gupta27 <guptasaloni.2016@gmail.com>
2025-11-07 13:26:20 +0530

    import correction

--- src/components/CakeDetails.jsx
+++ src/components/CakeDetails.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { addtocart } from "../redux/cartslice.js";
+import { addtocart } from "../redux/cartSlice.js";
 import { useDispatch } from "react-redux";
 
 const CakeDetails = ({ cartItems, setCartItems }) => {
